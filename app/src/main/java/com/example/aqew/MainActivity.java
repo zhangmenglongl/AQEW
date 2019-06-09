59e6d2b5be5d368a9c0eea00fa4f609228d1217e
day01 <you@example.com>
2019-06-09 20:39:32 +0800

    1234

--- app/src/main/java/com/example/aqew/MainActivity.java
+++ app/src/main/java/com/example/aqew/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.aqew;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = new Button(this, null);
+        TextView textView = new TextView(this, null);
     }
 }
